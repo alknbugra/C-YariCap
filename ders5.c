fb8a41927f97a524bea178dcb65dbcecdd675bef
Bugra Alkin <alknbugra@gmail.com>
2019-05-19 22:48:25 +0300

    Add files via upload

---
+++ ders5.c
@@ -0,0 +1,14 @@
+// ders 5
+#include<stdio.h>
+#define PI 3.1415
+#define alan(r) (PI*(r)*(r))
+
+int main(){
+	int yaricap;
+	float alan;
+	printf("Enter the yaricap : ");
+	scanf("%d",&yaricap);
+	alan=alan(yaricap);
+	printf("Alan=%.2f",alan);
+	return 0;
+}
